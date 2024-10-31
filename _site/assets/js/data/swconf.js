badc71d71b1c13d3ef8ba620fde810486d013632
Jiwon <wldnjs5818@naver.com>
2024-10-31 17:40:54 +0900

    <init> jekyll 블로그 설정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730363300',resources: [
+    cacheName: 'chirpy-1730364028',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

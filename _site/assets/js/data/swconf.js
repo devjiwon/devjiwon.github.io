4be47d4f26a23cc9f9ffd61bfc0dba9ff647da08
Jiwon <wldnjs5818@naver.com>
2024-10-31 17:48:46 +0900

    <fix> 아바타 설정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730364028',resources: [
+    cacheName: 'chirpy-1730364502',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

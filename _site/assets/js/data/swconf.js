63334b4d8937c088d0adbfc3eb6f18c8206cdea9
Jiwon <wldnjs5818@naver.com>
2024-11-17 16:06:04 +0900

    <fix: build 에러 수정>

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731826088',resources: [
+    cacheName: 'chirpy-1731827108',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
